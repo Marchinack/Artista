e2f987c17ffaf7941477af215bb8967c20aef087
Marchinack <167545286+Marchinack@users.noreply.github.com>
2024-05-13 14:37:31 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("-------");
+    System.out.println("-Queen-");
+    System.out.println("-------");
+
+    System.out.println("");
+  
+    System.out.println("Album: A night at the opera");
+    System.out.println("Ano de lançamento: 1975");
+    System.out.println("");
+    System.out.println("História: A Night at the Opera é um álbum lançado em 1975 pelo Queen. Seu nome foi inspirado no filme dos Irmãos Marx. A gravação foi cara e complexa, com destaque para a faixa Bohemian Rhapsody, que desafiou as convenções musicais da época. Apesar das dúvidas da gravadora, a música se tornou um enorme sucesso, ajudando a solidificar o Queen como uma das maiores bandas de rock da história.");
+    System.out.println("");
+    System.out.println("Músicas:");
+    System.out.println("");
+  }
+}
