f12141a64f2128ba2a92de0e5e9ab2e88c31203f
Marchinack <167545286+Marchinack@users.noreply.github.com>
2024-05-13 15:02:29 +0000

    13 de maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,22 @@ public class Main {
     System.out.println("História: A Night at the Opera é um álbum lançado em 1975 pelo Queen. Seu nome foi inspirado no filme dos Irmãos Marx. A gravação foi cara e complexa, com destaque para a faixa Bohemian Rhapsody, que desafiou as convenções musicais da época. Apesar das dúvidas da gravadora, a música se tornou um enorme sucesso, ajudando a solidificar o Queen como uma das maiores bandas de rock da história.");
     System.out.println("");
     System.out.println("Músicas:");
-    System.out.println("");
+    System.out.println("            Nome              Duração");
+       
+         
+    System.out.println("1     Death on Two legs        3:43");
+    System.out.println("2 Lazing on a sunday afternoon 1:15");
+    System.out.println("3 I'm in love with my car      3:16");
+    System.out.println("4 You're my best friend        2:52");
+    System.out.println("5       39                     3:34");
+    System.out.println("6  Sweet lady                  4:01");
+    System.out.println("7 Seaside Rendezvous           2:13");
+    System.out.println("8 The prophet's Song           8:21");
+    System.out.println("9 Love of my life              3:38");
+    System.out.println("10 Good company                3:23");
+    System.out.println("11 Bohemian Rhapsody           5:55");
+    System.out.println("12 God Save the Queen          1:15");
+    
+ 
   }
 }
